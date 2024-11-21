08b24bcb689da680b67bbc3da0cb763940543df6
MauriceDev1 <mrvolkwyn@gmail.com>
2024-11-21 17:40:58 +0200

    fixed the data

--- app/(root)/(booking)/province/page.tsx
+++ app/(root)/(booking)/province/page.tsx
@@ -1,9 +1,26 @@
+import southAfricaProvinces from '@/data/rsa';
 import React from 'react'
 
 const Province = () => {
+
   return (
     <div>
-        Province
+        {southAfricaProvinces.length > 0
+          ?
+            (
+              <>
+                {southAfricaProvinces.map(p => (
+                  <div key={p.province}>
+                    {p.province}
+                  </div>
+                ))}
+              </>
+            )
+          :
+            (
+              <p>There is no data available</p>
+            )
+        }
     </div>
   )
 }
---
+++ data/rsa.ts
@@ -0,0 +1,98 @@
+const southAfricaProvinces = [
+    {
+      province: "Eastern Cape",
+      cities: [
+        "Port Elizabeth",
+        "East London",
+        "Mthatha",
+        "Grahamstown",
+        "Queenstown",
+        "Bisho"
+      ]
+    },
+    {
+      province: "Free State",
+      cities: [
+        "Bloemfontein",
+        "Welkom",
+        "Sasolburg",
+        "Kroonstad",
+        "Phuthaditjhaba"
+      ]
+    },
+    {
+      province: "Gauteng",
+      cities: [
+        "Johannesburg",
+        "Pretoria",
+        "Sandton",
+        "Midrand",
+        "Boksburg",
+        "Ekurhuleni"
+      ]
+    },
+    {
+      province: "KwaZulu-Natal",
+      cities: [
+        "Durban",
+        "Pietermaritzburg",
+        "Newcastle",
+        "Richards Bay",
+        "Empangeni",
+        "Umlazi"
+      ]
+    },
+    {
+      province: "Limpopo",
+      cities: [
+        "Polokwane",
+        "Thohoyandou",
+        "Makhado",
+        "Lephalale",
+        "Bela-Bela"
+      ]
+    },
+    {
+      province: "Mpumalanga",
+      cities: [
+        "Nelspruit",
+        "Witbank",
+        "Piet Retief",
+        "Secunda",
+        "Lydenburg"
+      ]
+    },
+    {
+      province: "North West",
+      cities: [
+        "Mahikeng",
+        "Klerksdorp",
+        "Rustenburg",
+        "Potchefstroom",
+        "Zeerust"
+      ]
+    },
+    {
+      province: "Northern Cape",
+      cities: [
+        "Kimberley",
+        "Upington",
+        "Postmasburg",
+        "De Aar",
+        "Springbok"
+      ]
+    },
+    {
+      province: "Western Cape",
+      cities: [
+        "Cape Town",
+        "Stellenbosch",
+        "George",
+        "Paternoster",
+        "Knysna",
+        "Piketberg"
+      ]
+    }
+  ];
+
+  export default southAfricaProvinces;
